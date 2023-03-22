9bbc659d4f1f44ee5d56beb9abe79e38d48448cc
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-03-22 16:56:16 -0300

    Function components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Formulario from './Components/Formulario';
+import Lista from './Components/Lista'
 
 function App() {
   return (
     <div className="App">
-      
+      <Formulario/>
+      <Lista />
     </div>
   );
 }
---
+++ src/Components/Lista/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Lista() {
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, {
+        tarefa: 'JavaScript',
+        tempo: '01:00:00'
+    }];
+
+    return (
+        <aside>
+            <h2> Estudos do dia</h2>
+            <ul>
+                {tarefas.map((item, index) => (
+                    <li key={index}>
+                        <h3> {item.tarefa} </h3>
+                        <span> {item.tempo} </span>
+                    </li>
+                ))}
+
+                <li>
+                    <h3>
+                        React
+                    </h3>
+                    <span>
+                        02:00:00
+                    </span>
+                </li>
+                <li>
+                    <h3>
+                        JavaScript
+                    </h3>
+                    <span>
+                        01:00:00
+                    </span>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
