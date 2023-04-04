e269e5a28573c6380cd80ab9afa1e1249947f79a
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-04-04 10:13:29 -0300

    Extend Interfaces, Tenarios

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,14 @@
 import Botao from "../Botao";
 import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
 
-export default function Cronometro() {
+interface Props {
+    selecionado: ITarefa 
+}
+export default function Cronometro({selecionado}: Props) {
+    console.log('conversao:', tempoParaSegundos('01:01:01'));
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}> Escolha um card e inicie o cronômetro </p>
--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,10 +1,28 @@
-import { Itarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss'
+import { ITarefa } from '../../../types/tarefa';
+import style from './Item.module.scss'
 
-export default function Item({tarefa, tempo, selecionado, completado, id}: Itarefa) {
-    console.log('item atula:',{tarefa, tempo, selecionado, completado, id} );
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+export default function Item(
+    {
+        tarefa,
+        tempo,
+        selecionado,
+        completado,
+        id,
+        selecionaTarefa
+    }: Props) {
     return (
-        <li className={style.item}>
+        <li
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+            onClick={() => selecionaTarefa({
+                tarefa,
+                tempo,
+                selecionado,
+                completado,
+                id,
+            })}>
             <h3> {tarefa} </h3>
             <span> {tempo} </span>
         </li>
